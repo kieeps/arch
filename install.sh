fef32562fdf5f69e37bcc8c1b2b5340f66ff8b54
kieeps <kieeps@kieeps.com>
2021-10-30 17:38:53 +0200

    decided to stick with default KDE

--- install.sh
+++ install.sh
@@ -263,8 +263,7 @@ cp -R ~/arch/.zshrc /mnt/home/$username/
 cp -R ~/arch/.p10k.zsh /mnt/home/$username/
 arch-chroot /mnt chown -R $username:$username /home/$username/
 
-sed -i 's/^ColorScheme=BreezeLight/ColorScheme=BreezeDark/' /mnt/etc/paru.conf
-sed -i 's/^Name=Breeze Light/Name=BreezeDark/' /mnt/home/$username/.kde4/share/config/kdeglobals
+
 sed -i 's/^#BottomUp/BottomUp/' /mnt/etc/paru.conf
 
 echo -e ${RED}"-------------------------------------------------"
